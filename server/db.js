5d2a8a92b2e2318519d78eebb9d6b4dd1007e319
salmaa-maharani <salmaadm26@gmail.com>
2026-04-12 14:57:59 +0800

    update db config

--- server/db.js
+++ server/db.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '', // kosong kalau pakai XAMPP default
-  database: 'chat_db'
+  host: process.env.MYSQLHOST || 'localhost',
+  user: process.env.MYSQLUSER || 'root',
+  password: process.env.MYSQLPASSWORD || '',
+  database: process.env.MYSQLDATABASE || 'chat_db',
+  port: process.env.MYSQLPORT || 3306
 });
 
 db.connect((err) => {
